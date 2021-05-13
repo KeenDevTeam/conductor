50eb9b4818ecf7c93adefe2ff3260420a1d1678c
Danial Manavi <dmanavi@live.com>
2021-05-13 00:03:13 -0700

    Fix the switch method and an error message.

--- src/conductor.ts
+++ src/conductor.ts
@@ -10,16 +10,15 @@ import { runFactory } from './execution_engine';
 import ConditionalBlockFactory from './conditional_block';
 
 export type ConductorOptions = {
-  validateHandler?: HandlerValidator,
-  run?: ReturnType<typeof runFactory>,
-  conditionalBlockFactory?: ReturnType<typeof ConditionalBlockFactory>
+  validateHandler?: HandlerValidator;
+  run?: ReturnType<typeof runFactory>;
+  conditionalBlockFactory?: ReturnType<typeof ConditionalBlockFactory>;
 };
 
 /**
  * The heart of the conductor module
  */
 export default class Conductor implements Runnable {
-
   /**************************/
   /* Static method(s)       */
   /**************************/
@@ -27,20 +26,16 @@ export default class Conductor implements Runnable {
    * Create a new instance of the Conductor class
    * @returns New instance of the Conductor class
    */
-  static create(
-    options?: ConductorOptions,
-    ...handlers: Array<Handler>
-  ): Conductor { return new Conductor(options, ...handlers); }
+  static create(options?: ConductorOptions, ...handlers: Array<Handler>): Conductor {
+    return new Conductor(options, ...handlers);
+  }
   /**************************/
 
-  constructor(
-    options?: ConductorOptions,
-    ...handlers: Array<Handler>
-  ) {
+  constructor(options?: ConductorOptions, ...handlers: Array<Handler>) {
     this.debug('Creating a new instance of Conductor...');
 
-    if (typeof options !== 'object') {
-      throw new Error('Options must be an object.');
+    if (typeof options === 'undefined' || typeof options !== 'object') {
+      throw new Error('options must be an object.');
     }
 
     if (typeof options.validateHandler !== 'function') {
@@ -48,11 +43,11 @@ export default class Conductor implements Runnable {
     }
 
     if (typeof options.run !== 'function') {
-      throw new Error('options.run must be a function');
+      throw new Error('options.run must be a function.');
     }
 
     if (typeof options.conditionalBlockFactory !== 'function') {
-      throw new Error('options.conditionalBlockFactory must be a function');
+      throw new Error('options.conditionalBlockFactory must be a function.');
     }
 
     this.options = options;
@@ -88,7 +83,6 @@ export default class Conductor implements Runnable {
    * @returns Current instance of the conductor
    */
   public add(handler?: Handler, ...otherHandlers: Array<Handler>): Conductor {
-
     this.debug('Adding handler...');
     this.debug('Ensure that the handler is valid...');
 
@@ -101,7 +95,6 @@ export default class Conductor implements Runnable {
 
     // any other handler?
     if (otherHandlers.length > 0) {
-
       this.debug(`Adding total number of ${otherHandlers.length} handlers to the list...`);
 
       // iterate over other handlers, validate and add them to the list of children handlers
@@ -122,20 +115,11 @@ export default class Conductor implements Runnable {
    * @param cases List of cases
    * @returns Modified conductor
    */
-  public switch(
-    mainCase: ConditionalHandler,
-    ...otherCases: Array<ConditionalHandler>
-  ): Conductor {
-
+  public switch(mainCase: ConditionalHandler, ...otherCases: Array<DefaultConditionalHandler>): Conductor {
     this.debug(`Adding switch with total number of ${otherCases.length + 1} case(s)...`);
 
     // iterate over cases, convert them to a single conditional handler
-    this.handlers.push(
-      this.options.conditionalBlockFactory!(
-        mainCase,
-        ...otherCases
-      )
-    );
+    this.handlers.push(this.options.conditionalBlockFactory!(mainCase, ...otherCases));
 
     return this;
   }
@@ -145,20 +129,11 @@ export default class Conductor implements Runnable {
    * @param blocks List of if blocks
    * @returns Modified conductor
    */
-  public if(
-    mainBlock: ConditionalHandler,
-    ...otherBlocks: Array<DefaultConditionalHandler>
-  ): Conductor {
-
+  public if(mainBlock: ConditionalHandler, ...otherBlocks: Array<DefaultConditionalHandler>): Conductor {
     this.debug(`Adding if statement with total number of ${otherBlocks.length + 1} blocks(s)...`);
 
     // iterate over cases, convert them to a single conditional handler
-    this.handlers.push(
-      this.options.conditionalBlockFactory!(
-        mainBlock,
-        ...otherBlocks
-      )
-    );
+    this.handlers.push(this.options.conditionalBlockFactory!(mainBlock, ...otherBlocks));
 
     return this;
   }
@@ -175,27 +150,19 @@ export default class Conductor implements Runnable {
     let counter = 0;
 
     while (counter < this.handlers.length) {
-
       this.debug(`Running handler #${counter + 1} ...`);
 
       const isFirstHandler = counter === 0;
-      const handler = this.handlers[counter++];      
+      const handler = this.handlers[counter++];
 
       if (isFirstHandler === true) {
         // in the first iteration, all the parameters will be passed
         // using spread operator to handle unlimited number of parameters
-        result = await this.options.run!(
-          handler,
-          ...result
-        );
-      }
-      else {
+        result = await this.options.run!(handler, ...result);
+      } else {
         // for the rest of the iterations, result of the previous function
         // will be passed as a single item (functions returns 1 sinle item)
-        result = await this.options.run!(
-          handler,
-          result
-        );
+        result = await this.options.run!(handler, result);
       }
     }
 
